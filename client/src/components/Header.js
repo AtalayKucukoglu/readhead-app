08a23f51b306fc32b187162dc67ef3f1cb3ec8bc
Atalay Küçükoğlu <atalaykucukoglu@yahoo.com>
2021-01-13 15:51:59 +0300

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,33 @@
+import { Button, ButtonBase, Typography } from '@material-ui/core'
 import React, { Component } from 'react'
 import logo from '../images/logo.jpg'
 import profilePicture from '../images/profile_picture.jpg'
 import RoundedImage from './RoundedImage'
+import store from '../helpers/store'
 
 
 export default class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <img className='header-logo' src={logo} alt='Scoutium Logo'/>
-        <RoundedImage src={profilePicture} alt='Profile' height='80%' />
+        <div className='flex-row ai-center'>
+          <img className='header-logo' src={logo} alt='ReadHead Logo' onClick={() => this.goTo('/')}/>
+          {this.renderRouteItem('Login', '/login')}
+          {this.renderRouteItem('Register', '/register')}
+          {this.renderRouteItem('Search', '/search')}
+        </div>
+        <RoundedImage src={profilePicture} alt='Profile' height='80%' onClick={() => {}} />
       </div>
     )
   }
+
+  renderRouteItem(name, path) {
+    return (
+      <ButtonBase style={{padding: 20}} onClick={() => this.goTo(path)} >
+        <Typography variant='h6' style={{color:'#ffffff'}} >{name}</Typography>
+      </ButtonBase>
+    )
+  }
+
+  goTo = (path) => this.props.history.push(path)
 }
