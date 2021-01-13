14d0078630788832819cbecd00648d46fe47667f
Atalay Küçükoğlu <atalaykucukoglu@yahoo.com>
2021-01-13 15:26:54 +0300

    book search authors added

--- server/db/books.py
+++ server/db/books.py
@@ -1,5 +1,3 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import AsIs
 from server.db.connection import execute_statement
 from app import app
@@ -12,17 +10,21 @@ def get_book_with_id(book_id):
     data = execute_statement(statement, book_id, True)
     return jsonify(data)
 
-# FIXME
-@app.route('/api/books/search', methods=['GET'])
+@app.route('/api/books/search', methods=['POST'])
 def search_books():
-    req = request.json
-    statement = """select * from books where lower(title) like """
-    print(statement)
-    return "ok"
+    value = request.json['value'].lower()
+    print("SEARCH VALUE: ", value)
+    statement = """select b.*, a.* from
+                        books b join books_authors ba on b.book_id = ba.book_id
+                            join authors a on ba.author_id = a.author_id
+                                where lower(title) like """ + "'%%" + '%s' + "%%'"
+    response = execute_statement(statement, (AsIs(value),), True)
+    return jsonify(response), 200
+
 @app.route('/api/books/create', methods=['POST'])
 def create_book():
     req = request.json
 
-
+#TODO: filter books
 
 
