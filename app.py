74aec53d5849d36b43ab6665a9a7a409b281931f
Atalay Küçükoğlu <atalaykucukoglu@yahoo.com>
2021-01-20 03:24:53 +0300

    allow origin & static path public

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, send_from_directory
 from server.db.connection import db_connect
 import os
 
-app = Flask(__name__, static_folder='client/build', static_url_path='')
+app = Flask(__name__, static_folder='client/public', static_url_path='')
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
 
 import server.db.books
@@ -17,13 +17,14 @@ import server.db.authors
 def catch_all(path):
     print("inside catch all")
     if os.path.isfile('app/public/' + path):
-        return send_from_directory('client/build', path)
+        return send_from_directory('client/public', path)
 
     return app.send_static_file("index.html")
 
 @app.after_request
 def apply_caching(response):
-    response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    # response.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.set('Access-Control-Allow-Origin', 'https://readhead.herokuapp.com')
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     response.headers.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     return response
